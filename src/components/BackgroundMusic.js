18aa394c61060edd0e4cd7fceefb95c0d6e5ef77
sanjay <officialsanjay2002@gmail.com>
2025-11-22 18:42:53 -0700

    updated mp3 import path

--- src/components/BackgroundMusic.js
+++ src/components/BackgroundMusic.js
@@ -67,7 +67,7 @@ const BackgroundMusic = () => {
   return (
     <>
       <audio ref={audioRef} loop preload="auto" playsInline>
-        <source src={weddingSong} type="audio/mpeg" />
+        <source src={weddingSong} type="audio/mp3" />
       </audio>
 
       <div
