9de191302e71e5c7bbb1147dc99d47b15afa1b3d
Sanjay Karthikeyan <124025122+SanjayKarthikeyan002@users.noreply.github.com>
2025-11-22 21:34:36 -0700

    change2.0

--- src/components/BackgroundMusic.js
+++ src/components/BackgroundMusic.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import weddingSong from '../assets/images/EnMymaPeru';
+import weddingSong from '../assets/images/EnMymaPeru.mp3';
 
 const BackgroundMusic = () => {
   const [isPlaying, setIsPlaying] = useState(false);
